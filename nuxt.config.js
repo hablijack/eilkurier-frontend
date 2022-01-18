aedc6df20a4f411f24963ffb2f566b0d7898c11d
hablijack <christoph.habel@posteo.de>
2022-01-18 09:43:21 +0100

    Fixed backend url

--- nuxt.config.js
+++ nuxt.config.js
@@ -92,7 +92,6 @@ export default {
   ],
 
   publicRuntimeConfig: {
-    //backendUrl: 'https://eilkurier-backend.herokuapp.com/api'
-    backendUrl: 'http://localhost:8080/api'
+    backendUrl: 'https://eilkurier-backend.herokuapp.com/api'
   }
 }
